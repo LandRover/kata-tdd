7a0ceccabf9567f421fdaca707dfd20bbdbe3eb3
LandRover <github.com@landroverz.com>
2017-07-16 08:42:52 +0000

    added the input of 3 - expected to return [3] - test failing

--- tests/functions/prime_factors/prime_factors.spec.js
+++ tests/functions/prime_factors/prime_factors.spec.js
@@ -5,6 +5,7 @@ describe('Prime Factors', () => {
     it('should calculate prime factor of an int', () => {
         expect(PrimeFactors.of(1)).to.deep.equal([]);
         expect(PrimeFactors.of(2)).to.deep.equal([2]);
+        expect(PrimeFactors.of(3)).to.deep.equal([3]);
     });
 
 });
