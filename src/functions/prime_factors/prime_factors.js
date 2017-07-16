a223d8db97cc7dd6ef0c3e3ab580dc8f5bd5a668
LandRover <github.com@landroverz.com>
2017-07-16 09:07:05 +0000

    eliminated the first loop of 1 < n to a while - test still passes

--- src/functions/prime_factors/prime_factors.js
+++ src/functions/prime_factors/prime_factors.js
@@ -2,14 +2,13 @@ class PrimeFactors {
     of(n) {
         let result = [];
         
-        if (1 < n) {
+        while (1 < n) {
             if (0 === n % 2) {
                 result.push(2);
                 n /= 2;
-            }
-            
-            if (1 < n) {
+            } else {
                 result.push(n);
+                break;
             }
         }
 
