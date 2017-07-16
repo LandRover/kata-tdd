83163a3f34350515d63a683a3a338672f69fc4d8
LandRover <github.com@landroverz.com>
2017-07-16 09:07:47 +0000

    added test for prime of 5 - should return [5], test passes

--- tests/functions/prime_factors/prime_factors.spec.js
+++ tests/functions/prime_factors/prime_factors.spec.js
@@ -7,6 +7,7 @@ describe('Prime Factors', () => {
         expect(PrimeFactors.of(2)).to.deep.equal([2]);
         expect(PrimeFactors.of(3)).to.deep.equal([3]);
         expect(PrimeFactors.of(4)).to.deep.equal([2, 2]);
+        expect(PrimeFactors.of(5)).to.deep.equal([5]);
     });
 
 });
