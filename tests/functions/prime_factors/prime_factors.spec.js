e0beeefec818699b40b4a835907d090abfd15fb4
LandRover <github.com@landroverz.com>
2017-07-16 08:36:42 +0000

    added first expect, should return empty array for 1 prime

--- tests/functions/prime_factors/prime_factors.spec.js
+++ tests/functions/prime_factors/prime_factors.spec.js
@@ -3,7 +3,7 @@ import PrimeFactors from '../../../src/functions/prime_factors/prime_factors.js'
 describe('Prime Factors', () => {
 
     it('should calculate prime factor of an int', () => {
-        
+        expect(PrimeFactors.of(1)).to.deep.equal([]);
     });
 
 });
