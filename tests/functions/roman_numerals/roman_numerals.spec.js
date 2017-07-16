1ef5946aa8fd1955cece88b7844538ac123465e4
LandRover <github.com@landroverz.com>
2017-07-16 11:38:07 +0000

    added assert for 4, fails

--- tests/functions/roman_numerals/roman_numerals.spec.js
+++ tests/functions/roman_numerals/roman_numerals.spec.js
@@ -6,6 +6,7 @@ describe('Roman Numerals', () => {
         expect(RomanNumerals.fromNumber(1)).to.equal('I');
         expect(RomanNumerals.fromNumber(2)).to.equal('II');
         expect(RomanNumerals.fromNumber(3)).to.equal('III');
+        expect(RomanNumerals.fromNumber(4)).to.equal('IV');
     });
 
 });
