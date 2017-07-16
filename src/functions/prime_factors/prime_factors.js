6b2e88470b4a647dda98a633099f22061183e4aa
LandRover <github.com@landroverz.com>
2017-07-16 08:56:52 +0000

    slight addition for finding that case but still missing an array to collect matches

--- src/functions/prime_factors/prime_factors.js
+++ src/functions/prime_factors/prime_factors.js
@@ -1,6 +1,12 @@
 class PrimeFactors {
     of(n) {
-        if (1 < n) return [n];
+        if (1 < n) {
+            if (0 === n % 2) {
+                return [n];
+            }
+            
+            return [n];
+        }
 
         return [];
     }
