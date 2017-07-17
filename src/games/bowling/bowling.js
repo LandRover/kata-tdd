12fe7007cfce4adfc713291256f8feeacd973c07
LandRover <github.com@landroverz.com>
2017-07-17 14:29:09 +0000

    added some comments

--- src/games/bowling/bowling.js
+++ src/games/bowling/bowling.js
@@ -1,16 +1,32 @@
 /**
+ * Basic logic kata for Bowling game
  *
+ * @see: http://butunclebob.com/files/downloads/Bowling%20Game%20Kata.ppt
  */
 class Bowling {
 
+    /** Throws array, collects all throws history **/
     throws = [];
 
 
+    /**
+     * Adds the round result to throws.
+     *
+     * @param {Number} pins
+     * @returns {Object} this
+     */
     addThrow(pins) {
         this.throws.push(pins);
+
+        return this;
     }
 
 
+    /**
+     * Getter for the game score
+     *
+     * @returns {Number}
+     */
     getScore() {
         let score = 0,
             gameFrames = 10;
@@ -31,20 +47,30 @@ class Bowling {
             }
         }
 
-
         return score;
     }
 
 
-    _isSpare(throwIndex) {
-        return 10 === this.throws[throwIndex] + this.throws[throwIndex + 1];
-    }
-
-
+    /**
+     * Getter for the condition of a strike
+     *
+     * @param {Number} throwIndex
+     * @returns {Boolean}
+     */
     _isStrike(throwIndex) {
         return 10 === this.throws[throwIndex];
     }
 
+
+    /**
+     * Getter for the condition of a spare
+     *
+     * @param {Number} throwIndex
+     * @returns {Boolean}
+     */
+    _isSpare(throwIndex) {
+        return 10 === this.throws[throwIndex] + this.throws[throwIndex + 1];
+    }
 
 }
 
